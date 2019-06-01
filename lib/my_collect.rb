c0b3a6f22fd6538f011b8d9ce06b7bd1a93e8d2d
Alex Nahimana <alex.nahimana1@gmail.com>
2019-06-01 17:54:39 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -13,7 +13,7 @@ def my_collect(array)
   end
 end
 
-array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
-my_collect(array) do |name|
-  puts name.split(" ").first
-end
+# array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+# my_collect(array) do |name|
+#   puts name.split(" ").first
+# end
